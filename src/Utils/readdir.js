8f08788184893ea323f7e09cc4e257264ef9d42f
wioenena-q <brs_kprl09@outlook.com>
2021-09-19 21:28:14 +0300

    feat: Created a function that returns all files.

---
+++ src/Utils/readdir.js
@@ -0,0 +1,29 @@
+const { readdirSync, statSync } = require("fs");
+const { join } = require("path");
+
+
+/**
+ * Returns all files in the specified folder as an array.
+ * @param {string} path 
+ * @returns {string[]}
+ */
+module.exports = (path) => {
+    const paths = [];
+
+    const read = (_path) => {
+        const files = readdirSync(_path);
+
+        for (let i = 0; i < files.length; i++) {
+            const resolvedPath = join(_path, files[i]);
+
+            if (statSync(resolvedPath).isDirectory())
+                read(resolvedPath)
+            else
+                paths.push(resolvedPath);
+        }
+    }
+
+    read(path);
+
+    return paths;
+}
