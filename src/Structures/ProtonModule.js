7f902af90941bc82e3fcfa0f6f1a47f131e073e6
wioenena-q <brs_kprl09@outlook.com>
2021-09-20 07:48:53 +0300

    doc: Added JSDoc

--- src/Structures/ProtonModule.js
+++ src/Structures/ProtonModule.js
@@ -1,21 +1,50 @@
 class ProtonModule {
+    /**
+     *
+     * @param {string} id - ID of the module.
+     * @param {ProtonModuleOptions} options - Options for the module.
+     */
     constructor(id, options = {}) {
         if (typeof id !== "string") {
             throw new TypeError("The module ID must be a type of string.");
         }
 
+        /** @type {string} */
         this.id = id;
 
+        /** @type {string} */
         this.category = typeof options.category === "string"
             ? options.category
             : "default";
 
+        /**
+         * The module's client class.
+         * @type {ProtonClient!}
+         */
         this.client = null;
 
+        /**
+         * The module's handler class.
+         * @type {ProtonHandler!}
+         */
         this.handler = null;
 
+        /**
+         * The file path of the module, if any.
+         * @type {string?}
+         */
         this.filepath = null;
     }
 }
 
 module.exports = ProtonModule;
+
+/**
+ * @typedef {object} ProtonModuleOptions
+ * @property {string} [category="default"] - Category of the module.
+ */
+
+/**
+ * @typedef {import("./ProtonClient")} ProtonClient
+ * @typedef {import("./ProtonHandler")} ProtonHandler
+ */
