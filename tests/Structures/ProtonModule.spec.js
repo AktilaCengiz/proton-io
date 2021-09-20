d633d2918f91c5c0aae2daaee2283727464b017b
wioenena-q <brs_kprl09@outlook.com>
2021-09-20 09:11:28 +0300

    test: ProtonModule class tested.

---
+++ tests/Structures/ProtonModule.spec.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+
+const ProtonClient = require("../../src/Structures/ProtonClient");
+const ProtonHandler = require("../../src/Structures/ProtonHandler");
+const ProtonModule = require("../../src/Structures/ProtonModule");
+
+class Client extends ProtonClient {
+    constructor() {
+        super({
+            intents: []
+        });
+
+        this.handler = new ProtonHandler(this, {
+            directory: ""
+        });
+    }
+}
+
+const client = new Client();
+
+beforeEach(client.handler.removeAll.bind(client.handler));
+
+test("ProtonModule", () => {
+    const MyFirstModule = new ProtonModule("MyFirstModule", {
+        category: "Software"
+    });
+
+    const MySecondModule = new ProtonModule("MySecondModule");
+
+    const [registered, registeredSecond] = [client.handler.register(MyFirstModule, null), client.handler.register(MySecondModule)];
+
+    expect(registered.id).toBe("MyFirstModule");
+    expect(registered.category).toBe("Software");
+    expect(registered.filepath).toBeNull();
+    expect(registered.client).toBe(client);
+    expect(registered.handler).toBe(client.handler);
+    expect(typeof registered.reload).toBe("function");
+    expect(typeof registered.remove).toBe("function");
+    expect(registeredSecond.category).toBe("default");
+    expect(client.handler.modules.size).toBe(2);
+    expect(registered).toMatchSnapshot();
+    expect(registeredSecond).toMatchSnapshot();
+    registered.remove();
+    registeredSecond.remove();
+    expect(client.handler.modules.size).toBe(0);
+    expect(client.handler.modules.random()).toBeUndefined();
+});
