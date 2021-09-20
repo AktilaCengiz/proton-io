3752e8c5d5e0518d76e3608608f21299845587c0
wioenena-q <brs_kprl09@outlook.com>
2021-09-20 08:15:48 +0300

    chore: ProtonClient options also get discord.js Client options

--- src/Structures/ProtonClient.js
+++ src/Structures/ProtonClient.js
@@ -1,14 +1,24 @@
 const { Client } = require("discord.js");
 
 class ProtonClient extends Client {
+    /**
+     *
+     * @param {ProtonClientOptions & ClientOptions} options - Options for the ProtonClient.
+     * @param {ClientOptions} clientOptions - Options for the discord.js Client.
+     */
     constructor(options, clientOptions) {
-        super(clientOptions);
+        super({ ...options, ...clientOptions });
 
         this.owners = options.owners instanceof Array
             ? options.owners
             : [];
     }
 
+    /**
+     * Returns whether the specified person is the owner of the bot.
+     * @param {UserResolvable} resolvable - User or User id
+     * @returns {boolean}
+     */
     isOwner(resolvable) {
         const user = this.users.resolve(resolvable);
 
@@ -21,3 +31,13 @@ class ProtonClient extends Client {
 }
 
 module.exports = ProtonClient;
+
+/**
+ * @typedef {object} ProtonClientOptions
+ * @property {string|string[]} [owners=[]] - Owner(s) of the client.
+ */
+
+/**
+ * @typedef {import("discord.js").ClientOptions} ClientOptions
+ * @typedef {import("discord.js").UserResolvable} UserResolvable
+ */
