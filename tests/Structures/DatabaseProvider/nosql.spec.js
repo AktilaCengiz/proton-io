a0c0f2374f9a87705eeb4a05ba1efa266f04cfb6
aktilacengiz <fbgstr38@gmail.com>
2021-09-22 16:17:39 +0300

    Edited package.json

--- tests/Structures/DatabaseProvider/nosql.spec.js
+++
@@ -1,9 +0,0 @@
-const { JsonDatabase } = require("wio.db");
-
-const db = new JsonDatabase({
-    databasePath: "./myJsonDatabase.json"
-});
-
-// Data set | get
-db.set("test", 1);
-db.get("test");
