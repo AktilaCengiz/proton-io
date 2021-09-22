5f2b0477624d6bc196016a4f2e0eaf27ecaf2bfb
wioenena-q <brs_kprl09@outlook.com>
2021-09-22 22:31:21 +0300

    feat: The command(s) is running (simple)

--- src/Structures/Commands/CommandHandler.js
+++ src/Structures/Commands/CommandHandler.js
@@ -1,4 +1,6 @@
+const isAsync = require("../../Utils/isAsync");
 const ProtonHandler = require("../ProtonHandler");
+const AliasManager = require("./AliasManager");
 
 class CommandHandler extends ProtonHandler {
     /**
@@ -9,8 +11,8 @@ class CommandHandler extends ProtonHandler {
     constructor(client, options) {
         super(client, options);
 
-        /** @type {string!} */
-        this.prefix = typeof options.prefix === "string"
+        /** @type {(string | PrefixBuilder)!} */
+        this.prefix = typeof options.prefix === "string" || typeof options.prefix === "function"
             ? options.prefix
             : "!";
 
@@ -25,9 +27,127 @@ class CommandHandler extends ProtonHandler {
             : true;
 
         /** @type {number?} */
-        this.defaultCooldown = typeof options.defaultCooldown === "function"
+        this.defaultCooldown = typeof options.defaultCooldown === "number"
             ? options.defaultCooldown
             : null;
+
+        this.aliasManager = new AliasManager();
+
+        this.init();
+    }
+
+    /**
+     *
+     * @param {Command} mod - Command
+     * @returns {void}
+     */
+    register(mod) {
+        super.register(mod);
+
+        if (typeof mod.aliases === "string" || mod.aliases instanceof Array) {
+            if (mod.aliases instanceof Array) {
+                for (let i = 0; i < mod.aliases.length; i++)
+                    this.aliasManager.register(mod.aliases[i], mod.id);
+            } else
+                this.aliasManager.register(mod.aliases, mod.id);
+        }
+    }
+
+    /**
+     *
+     * @param {Command} mod - Command
+     * @returns {void}
+     */
+    deregister(mod) {
+        super.deregister(mod);
+
+        if (typeof mod.aliases === "string" || mod.aliases instanceof Array) {
+            if (mod.aliases instanceof Array) {
+                for (let i = 0; i < mod.aliases.length; i++)
+                    this.aliasManager.deregister(mod.aliases[i]);
+            } else
+                this.aliasManager.deregister(mod.aliases);
+        }
+    }
+
+    init() {
+        this.client.once("ready", () => {
+            this.client.on("messageCreate", async (message) => {
+                if (message.partial) await message.fetch();
+
+                await this.handle(message);
+            });
+        });
+    }
+
+    /**
+     *
+     * @param {Message} message - Message structure.
+     */
+    async handle(message) {
+        // Ignore bots.
+        if (this.ignoreBots && message.author.bot) return;
+        // Ignore self.
+        if (this.ignoreSelf && message.author.id === this.client.user.id) return;
+
+        /** @type {*} */
+        let { prefix } = this;
+
+        // Get prefix.
+        if (typeof this.prefix === "function") {
+            prefix = isAsync(this.prefix)
+                ? await this.prefix(message)
+                : this.prefix(message);
+        }
+
+        if (typeof prefix !== "string" || !message.content.startsWith(prefix))
+            return;
+
+        // Parse message content.
+        const [cmdName, ...args] = message.content.slice(prefix.length).split(/\s/g);
+
+        /** @type {*} */
+        const command = this.modules.get(this.aliasManager.cache.get(cmdName));
+
+        if (!command) return;
+
+        // Check if owner specific.
+        if (command.ownerOnly && !this.client.isOwner(message.author)) return;
+
+        // Check where to run
+        if (typeof command.whereRunning === "string" || typeof command.whereRunning === "function") {
+            if (typeof command.whereRunning === "function") {
+                const result = isAsync(command.whereRunning)
+                    ? await command.whereRunning(message)
+                    : command.whereRunning(message);
+
+                if (result !== null)
+                    return;
+            } else if (command.whereRunning === "guild") {
+                if (!message.guild) return;
+            } else if (command.whereRunning === "dmChannel") {
+                if (message.guild && message.channel.type !== "DM") return;
+            }
+        }
+
+        // Check member permission(s).
+        if (command.userPermissions instanceof Array || typeof command.userPermissions === "string") {
+            if (!message.member.permissions.has(command.userPermissions))
+                return;
+        }
+
+        // Check client permission.
+        if (command.botPermissions instanceof Array || typeof command.botPermissions === "string") {
+            if (!message.guild.me.permissions.has(command.botPermissions))
+                return;
+        }
+
+        // Run async
+        if (isAsync(command.execute)) {
+            await command.execute(message);
+        } else {
+            command.execute(message);
+        }
     }
 }
 
@@ -43,10 +163,11 @@ module.exports = CommandHandler;
 
 /**
  * @typedef {import("discord.js").Message} Message
+ * @typedef {import("./Command")} Command
  * @typedef {import("../ProtonClient")} ProtonClient
  * @typedef {import("../ProtonHandler").ProtonHandlerOptions} ProtonHandlerOptions
  */
 
 /**
- * @typedef {string | string[] | {(message:Message): string | string[]}} PrefixBuilder
+ * @typedef {string | string[] | {(message:Message): string | string[] | Promise<string | string[]>}} PrefixBuilder
  */
