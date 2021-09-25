ee4183482426530875128a9a9347926cc8315c67
wioenena-q <brs_kprl09@outlook.com>
2021-09-25 23:18:41 +0300

    feat: Added 'cooldown' event.

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,7 +1,8 @@
 const CommandHandlerEvents = {
     COMMAND_NOT_EXECUTABLE: "commandNotExecutable",
     COΜMAND_NOT_FOUND: "commandNotFound",
-    MISSING_PERMISSIONS: "missingPermissions"
+    MISSING_PERMISSIONS: "missingPermissions",
+    COOLDOWN: "cooldown"
 };
 
 module.exports = {
