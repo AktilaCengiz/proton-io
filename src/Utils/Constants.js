92dde27d2dca4a668c2d8d9e1177ed87703e1e93
wioenena-q <brs_kprl09@outlook.com>
2021-09-25 21:10:03 +0300

    feat: Constants has been created.

---
+++ src/Utils/Constants.js
@@ -0,0 +1,9 @@
+const CommandHandlerEvents = {
+    COMMAND_NOT_EXECUTABLE: "commandNotExecutable",
+    COΜMAND_NOT_FOUND: "commandNotFound",
+    MISSING_PERMISSIONS: "missingPermissions"
+};
+
+module.exports = {
+    CommandHandlerEvents
+};
