a5b0bd456f275cbd995bb4d133bcf89b11f0580d
wioenena-q <brs_kprl09@outlook.com>
2021-09-30 22:11:56 +0300

    chore: moved to Types.js

--- src/Utils/isAsync.js
+++
@@ -1,9 +0,0 @@
-const noop = (async () => {}).constructor;
-
-/**
- * Returns whether the function is asynchronous.
- * @param {Function} value - Target value.
- * @returns {boolean}
- */
-module.exports = (value) => Object.prototype.toString.call(value) === "[object AsyncFunction]"
-    && value instanceof noop;
