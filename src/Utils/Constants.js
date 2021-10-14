b094c44e1d27d7e06cd5d97f3c8901ab82813d20
wioenena-q <brs_kprl09@outlook.com>
2021-10-14 15:29:50 +0300

    feat: Added events for CommandRunner class.

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,11 +1,17 @@
 const CommandHandlerEvents = {
-    COMMAND_NOT_EXECUTABLE: "commandNotExecutable",
     COΜMAND_NOT_FOUND: "commandNotFound",
+    COMMAND_START: "commandStarted",
+    COMMAND_END: "commandFinished",
+    ERROR: "error"
+};
+const CommandRunnerEvents = {
+    COMMAND_NOT_EXECUTABLE: "commandNotExecutable",
     MISSING_PERMISSIONS: "missingPermissions",
     COOLDOWN: "cooldown",
     ERROR_AFTER_COMMAND_RUN: "errorAfterCommandRun"
-};
+}
 
 module.exports = {
-    CommandHandlerEvents
+    CommandHandlerEvents,
+    CommandRunnerEvents
 };
