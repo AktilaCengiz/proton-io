e86455a7e471309ae698c335e85db9cb9081a6ea
wioenena-q <brs_kprl09@outlook.com>
2021-11-06 19:41:31 +0300

    doc: Added JSDoc

--- src/Structures/Listeners/Listener.js
+++ src/Structures/Listeners/Listener.js
@@ -1,9 +1,43 @@
+const { isString } = require("../../Utils/Types");
 const ProtonModule = require("../ProtonModule");
 
+/**
+ * @extends {ProtonModule}
+ */
 class Listener extends ProtonModule {
-    constructor(id, options = {}) {
+    /**
+     *
+     * @param {string} id
+     * @param {ListenerOptions & ProtonModuleOptions} options
+     */
+    constructor(id, options) {
         super(id, options);
+
+        if (!isString(options?.emitter))
+            throw new TypeError("The emitter must be of string type.");
+
+        if (!isString(options?.event))
+            throw new TypeError("The event must be of string type.");
+
+        /** @type {string} */
+        this.emitter = options.emitter;
+
+        /** @type {string} */
+        this.event = options.event;
+
+        /** @type {string} */
+        this.type = isString(options?.type)
+            ? options.type
+            : "on";
     }
 }
 
 module.exports = Listener;
+
+/**
+ * @typedef {import("../ProtonModule").ProtonModuleOptions} ProtonModuleOptions
+ * @typedef {object} ListenerOptions
+ * @property {string} emitter - The event emitter, either a key from ListenerHandler#emitters
+ * @property {string} event - Event name to listen to.
+ * @property {string} [type="on"] - Type of listener.
+*/
--- src/Structures/Listeners/ListenerHandler.js
+++ src/Structures/Listeners/ListenerHandler.js
@@ -1,5 +1,78 @@
+const { Collection } = require("discord.js");
+const { EventEmitter } = require("events");
 const ProtonHandler = require("../ProtonHandler");
 
-class ListenerHandler extends ProtonHandler { }
+class ListenerHandler extends ProtonHandler {
+    constructor(client, options) {
+        super(client, options);
+
+        /** @type {Collection<string,EventEmitter>} */
+        this.emitters = new Collection();
+
+        this.setEmitter("client", this.client);
+    }
+
+    /**
+     * 
+     * @param {Listener} listener 
+     * @param {string?} [filepath] 
+     */
+    register(listener, filepath) {
+        super.register(listener, filepath);
+
+        listener.execute = listener.execute.bind(listener);
+
+        const emitter = this.emitters.get(listener.emitter);
+
+        if (!emitter)
+            throw new Error(`Emitter with id '${listener.emitter}' not found.`);
+
+        if (listener.type === "once") {
+            emitter.once(listener.event, listener.execute);
+        } else if (listener.type === "on") {
+            emitter.on(listener.event, listener.execute);
+        }
+    }
+
+    /**
+     * 
+     * @param {Listener} listener 
+     */
+    deregister(listener) {
+        super.deregister(listener);
+        const emitter = this.emitters.get(listener.emitter);
+
+        emitter.removeListener(listener.event, listener.execute);
+    }
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {EventEmitter} emitter 
+     */
+    setEmitter(id, emitter) {
+        if (!(emitter instanceof EventEmitter))
+            throw new TypeError("Emitter does not match EventEmitter.");
+
+        this.emitters.set(id, emitter);
+    }
+
+    /**
+     * 
+     * @param {object} emitters 
+     * @returns {this}
+     */
+    setEmitters(emitters) {
+        for (const id in emitters) {
+            this.setEmitter(id, emitters[id]);
+        }
+        return this;
+    }
+
+}
 
 module.exports = ListenerHandler;
+
+/**
+ * @typedef {import("./Listener")} Listener
+ */
